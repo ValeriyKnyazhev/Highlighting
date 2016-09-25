0e1753a5360f698ef4265a5fd76c48e49bbca18c
Княжев Валерий <valeriy.knyazhev@fivt.in.ru>
2016-09-25 18:05:25 +0300

    added main functional definition without realization

--- ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutoCompleteon.cpp
+++ ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutoCompleteon.cpp
@@ -3,9 +3,18 @@
 
 ModuleHighlightingAndAutoCompleteon::ModuleHighlightingAndAutoCompleteon(std::string fileConfig) {
 	//открываем файл fileConfig, в котором содержатся ключевые слова, которые надо подсвечивать
-
+	readFileKeyWords(fileConfig);
 }
 
 ModuleHighlightingAndAutoCompleteon::ModuleHighlightingAndAutoCompleteon() {
 	
+}
+
+void ModuleHighlightingAndAutoCompleteon::readFileKeyWords(std::string) {
+
+}
+
+std::string ModuleHighlightingAndAutoCompleteon::getHighlightingTextString(std::string) {
+
+	return 0;
 }
--- ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutoCompleteon.h
+++ ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutoCompleteon.h
@@ -1,10 +1,21 @@
 #pragma once
 #include <string>
+#include <map>
 
 class ModuleHighlightingAndAutoCompleteon {
+private:
+	enum Color {
+		Red, Green, Blue
+	};
+	std::map<std::string, Color> KeyWords;//для каждого ключевого слова содержит номер цвета для отображения
 
-	ModuleHighlightingAndAutoCompleteon(std::string fileConfig);
+	void readFileKeyWords(std::string);
+
+public:
+	ModuleHighlightingAndAutoCompleteon(std::string);
 	ModuleHighlightingAndAutoCompleteon();
 
+	std::string getHighlightingTextString(std::string);
+	
 
 };
