42428b98da9e42b678b7486c8f082566f291345b
Княжев Валерий <valeriy.knyazhev@fivt.in.ru>
2016-09-25 17:32:46 +0300

    initial commit

---
+++ ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutoCompleteon.cpp
@@ -0,0 +1,11 @@
+#include "ModuleHighlightingAndAutoCompleteon.h"
+#include <string>
+
+ModuleHighlightingAndAutoCompleteon::ModuleHighlightingAndAutoCompleteon(std::string fileConfig) {
+	//открываем файл fileConfig, в котором содержатся ключевые слова, которые надо подсвечивать
+
+}
+
+ModuleHighlightingAndAutoCompleteon::ModuleHighlightingAndAutoCompleteon() {
+	
+}
---
+++ ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutoCompleteon.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+class ModuleHighlightingAndAutoCompleteon {
+
+	ModuleHighlightingAndAutoCompleteon(std::string fileConfig);
+	ModuleHighlightingAndAutoCompleteon();
+
+
+};
