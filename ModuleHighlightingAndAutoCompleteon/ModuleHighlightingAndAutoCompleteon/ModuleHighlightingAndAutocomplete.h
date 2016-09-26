fc49f48df0593136802c0dc3973f45db9f9ca5de
Fedor Alekseev <feodor.alexeev@gmail.com>
2016-09-27 02:33:20 +0300

    Use unordered_map: we don't really care about order

--- ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutocomplete.h
+++ ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutocomplete.h
@@ -1,13 +1,13 @@
 #pragma once
 #include <string>
-#include <map>
+#include <unordered_map>
 
 class ModuleHighlightingAndAutocomplete {
 private:
 	enum Color {
 		Red, Blue, Green
 	};
-	std::map<std::string, Color> keyWords;//for each keyword, map contains the color number for display in the XML file
+	std::unordered_map<std::string, Color> keyWords;//for each keyword, map contains the color number for display in the XML file
 	std::string stringKeyWords;
 	void readFileKeyWords();//read file with all keywords and colors for it
 	std::string parse(std::string text);//parse text file to xml with color font
