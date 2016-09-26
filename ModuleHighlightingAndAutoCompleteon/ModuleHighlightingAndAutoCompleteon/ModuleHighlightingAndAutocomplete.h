1fec85f43a0eecef3d29241d58278eeafab90fc7
valeriyknyazhev <valeriy.knyazhev@yandex.ru>
2016-09-27 01:04:20 +0300

    added comments

--- ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutocomplete.h
+++ ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutoCompleteon/ModuleHighlightingAndAutocomplete.h
@@ -7,17 +7,15 @@ private:
 	enum Color {
 		Red, Blue, Green
 	};
-	std::map<std::string, Color> keyWords;//для каждого ключевого слова содержит номер цвета для отображения
+	std::map<std::string, Color> keyWords;//for each keyword, map contains the color number for display in the XML file
 	std::string stringKeyWords;
-	void readFileKeyWords();
-	std::string parse(std::string);
+	void readFileKeyWords();//read file with all keywords and colors for it
+	std::string parse(std::string text);//parse text file to xml with color font
 
 public:
-	ModuleHighlightingAndAutocomplete(std::string);
+	ModuleHighlightingAndAutocomplete(std::string fileConfig);//set file with keywords
 	ModuleHighlightingAndAutocomplete();
 	
-	void HighlightText(std::string);
-
-	
+	void HighlightText(std::string fileName);//extract code with highlighting from file with name(fileName) to file with name("Highlighted_" + fileName)
 
 };
