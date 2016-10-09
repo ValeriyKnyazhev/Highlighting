312584b657bd1d4291679e44d13830b4f9923f7f
Fedor Alekseev <feodor.alexeev@gmail.com>
2016-10-09 19:27:45 +0300

    Add trie test

--- Highlighting/Highlighting/main.cpp
+++ Highlighting/Highlighting/main.cpp
@@ -7,20 +7,30 @@
 #include "IdentifierParser.h"
 #include "TestIdentifier.h"
 #include "TestImports.h"
+#include "TrieTest.h"
 
 using namespace Yapynb;
 
 int main() {
+	TrieTest();
+	std::cout << "Trie test was successful\n";
+
+	std::cout << (
+		(testIdentifiers1() == true)
+		? "Identifiers parser passed the test.\n"
+		: "Identifiers parser failed the test.\n"
+	);
+
+	TestImports::test();
+
+	std::cout << "Now enter the input for highlighter:\n";
+
 	std::string text{
 		std::istreambuf_iterator<char>(std::cin),
 		std::istreambuf_iterator<char>()
 	};
-
 	CHighlighting high(text);
 	high.OutputTagged(std::cout);
 	
-	std::cout << ((testIdentifiers1() == true) ? "Test is successfull completed.\n" : "Test is failed.\n");
-
-	TestImports::test();
 	return 0;
 }
