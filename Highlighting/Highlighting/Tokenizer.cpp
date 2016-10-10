4930e222de962dc781e00460d6fdceb3bc25d2f9
Fedor Alekseev <feodor.alexeev@gmail.com>
2016-10-11 01:38:32 +0300

    Workaround crazy MSVC implementation of std::regex

--- Highlighting/Highlighting/Tokenizer.cpp
+++ Highlighting/Highlighting/Tokenizer.cpp
@@ -17,18 +17,18 @@ std::vector<CToken> ITokenizer::Tokenize() {
 namespace {
 
 const auto RegexFlags =
-	std::regex_constants::ECMAScript
+    std::regex_constants::ECMAScript
 	| std::regex_constants::nosubs
 	| std::regex_constants::optimize
 ;
 
 const std::vector<std::pair<CToken::TType, std::regex>> TokenRegexps{
-	{CToken::TType::Comment, std::regex("^#.*\\n", RegexFlags)},
-	{CToken::TType::StringLiteral, std::regex(R"re(^('([^']|\')*')|("([^"]\')*"))re", RegexFlags)},
-	{CToken::TType::EndOfLine, std::regex("^\\n+", RegexFlags)},
-	{CToken::TType::Whitespace, std::regex("^[\\t ]+", RegexFlags)},
-	{CToken::TType::Identifier, std::regex(R"re(^(_|[[:alpha:]])\w*)re", RegexFlags)},
-	{CToken::TType::Number, std::regex(R"re(^(\d+(\.?\d*)|\.\d+)j?)re", RegexFlags)},
+	{CToken::TType::Comment, std::regex("#.*\\n", RegexFlags)},
+	{CToken::TType::StringLiteral, std::regex(R"re(('([^']|\')*')|("([^"]\')*"))re", RegexFlags)},
+	{CToken::TType::EndOfLine, std::regex("\\n+", RegexFlags)},
+	{CToken::TType::Whitespace, std::regex("[\\t ]+", RegexFlags)},
+	{CToken::TType::Identifier, std::regex(R"re(\w\w*)re", RegexFlags)},
+	{CToken::TType::Number, std::regex(R"re((\d+(\.?\d*)|\.\d+)j?)re", RegexFlags)},
 };
 
 }
@@ -47,16 +47,10 @@ public:
 		for (const auto& tokenRegexp : TokenRegexps) {
 			std::smatch matchedHere;
 			if (std::regex_search(begin, end, matchedHere, tokenRegexp.second)) {
-				auto bestHere = *std::max_element(
-					matchedHere.begin(),
-					matchedHere.end(),
-					[](const auto& a, const auto& b) {
-					return a.length() < b.length();
-				}
-				);
-				if (bestHere.length() > (int)best.Text.size()) {
+                std::ssub_match here = matchedHere[0];
+				if (here.first == begin && here.length() > (int)best.Text.size()) {
 					best.Type = tokenRegexp.first;
-					best.Text = bestHere.str();
+					best.Text = here.str();
 				}
 			}
 		}
