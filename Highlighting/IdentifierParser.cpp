2745b7bdfd40300967abab5f2352fae748a9b1ed
Nikita Rykov <nikita23angarsk@gmail.com>
2016-10-25 02:08:07 +0300

    Refctor ImportParser and IdentifierParser

--- Highlighting/IdentifierParser.cpp
+++ Highlighting/IdentifierParser.cpp
@@ -4,115 +4,128 @@
 #include "IdentifierParser.h"
 
 namespace Yapynb {
+	void IdentifierParser::processFor( 
+		const std::vector<std::pair<CToken, size_t>>& tokens, 
+		size_t cur, 
+		std::unordered_set<std::string>& identifiers 
+		) {
+		if (
+			tokens[cur].first.Type != CToken::TType::Identifier ||
+			tokens[cur].first.Text != "for"
+			) {
+			return;
+		}
+		cur++;
+		if (cur >= tokens.size() ||
+			tokens[cur].first.Type != CToken::TType::Whitespace
+			) {
+			return;
+		}
+		cur++;
+		while( cur < tokens.size() ) {
+			if( tokens[cur].first.Type == CToken::TType::Whitespace ) {
+				cur++;
+			}
+			if( 
+				cur < tokens.size() &&
+				tokens[cur].first.Type == CToken::TType::Identifier
+				) {
+				identifiers.insert( tokens[cur].first.Text );
+				cur++;
+			}
+			if(
+				cur < tokens.size() &&
+				tokens[cur].first.Type == CToken::TType::Whitespace
+				) {
+				cur++;
+			}
+			if(
+				cur >= tokens.size() ||
+				tokens[cur].first.Type != CToken::TType::Other ||
+				tokens[cur].first.Text != ","
+				) {
+				return;
+			} 
+			cur++;
+		}		
+	}
 
-	std::unordered_set<std::string> IdentifierParser::getIdentifiers( 
+	void IdentifierParser::processEquals( 
+		const std::vector<std::pair<CToken, size_t>>& tokens, 
+		size_t cur, 
+		std::unordered_set<std::string>& identifiers 
+		) {
+		if (tokens[cur].first.Type != CToken::TType::Other ||
+			tokens[cur].first.Text != "="
+			) {
+			return;
+		}
+		if( cur > 0 && tokens[cur - 1].first.Text == "=" ) {
+			return;
+		}
+		if( cur + 1 < tokens.size() && tokens[cur + 1].first.Text == "=" ) {
+			return;
+		}
+		cur--;
+		if( cur >= 0 &&
+			tokens[cur].first.Type == CToken::TType::Whitespace
+			) {
+			cur--;
+		}
+		while( cur >= 0 ) {
+			if( tokens[cur].first.Type == CToken::TType::Whitespace ) {
+				if( cur == 0 ) {
+					return;
+				}
+				cur--;
+			}
+			if( tokens[cur].first.Type == CToken::TType::Identifier ) {
+				identifiers.insert( tokens[cur].first.Text );
+				if( cur == 0 ) {
+					return;
+				}
+				cur--;
+			}
+			if( tokens[cur].first.Type == CToken::TType::Whitespace ) {
+				if( cur == 0 ) {
+					return;
+				}
+				cur--;
+			}
+			if(
+				tokens[cur].first.Type == CToken::TType::Other &&
+				tokens[cur].first.Text == ","
+				) {
+				if( cur == 0 ) {
+					return;
+				}
+				cur--;
+			} else {
+				return;
+			}
+		}
+	}
+
+	std::unordered_set<std::string> IdentifierParser::getIdentifiers(
 		const std::vector<std::pair<CToken, size_t>>& tokens 
 		) {
 		std::unordered_set<std::string> identifiers;
-
-		for( size_t i = 0; i < tokens.size(); ++i ) {
+		for (size_t i = 0; i < tokens.size(); ++i) {
 			size_t cur = i;
-			if(
-				tokens[cur].first.Type == CToken::TType::Identifier &&
-				tokens[cur].first.Text == "for"
-				) {
-				cur++;
-				if( cur < tokens.size() &&
-					tokens[cur].first.Type == CToken::TType::Whitespace
-					) {
-					cur++;
-					while( cur < tokens.size() ) {
-						if( tokens[cur].first.Type == CToken::TType::Whitespace ) {
-							cur++;
-						}
-						if( 
-							cur < tokens.size() &&
-							tokens[cur].first.Type == CToken::TType::Identifier
-							) {
-							identifiers.insert( tokens[cur].first.Text );
-							cur++;
-						}
-						if(
-							cur < tokens.size() &&
-							tokens[cur].first.Type == CToken::TType::Whitespace
-							) {
-							cur++;
-						}
-						if(
-							cur < tokens.size() &&
-							tokens[cur].first.Type == CToken::TType::Other &&
-							tokens[cur].first.Text == ","
-							) {
-							cur++;
-						} else {
-							break;
-						}
-					}
-				}
-			}		
-			if( 
+			processFor( tokens, cur, identifiers );
+			processEquals(  tokens, cur, identifiers );
+			if (
 				i + 2 < tokens.size() &&
 				tokens[i].first.Type == CToken::TType::Identifier &&
-				( tokens[i].first.Text == "class" || 
-					tokens[i].first.Text == "def" ) &&
+				(tokens[i].first.Text == "class" ||
+					tokens[i].first.Text == "def") &&
 				tokens[i + 1].first.Type == CToken::TType::Whitespace &&
 				tokens[i + 2].first.Type == CToken::TType::Identifier
 				) {
 				identifiers.insert( tokens[i + 2].first.Text );
 			}
-			cur = i;
-			if (
-				tokens[cur].first.Type == CToken::TType::Other &&
-				tokens[cur].first.Text == "="
-				) {
-				if( cur > 0 && tokens[cur - 1].first.Text == "=" ) {
-					continue;
-				}
-				if( cur + 1 < tokens.size() && tokens[cur + 1].first.Text == "=" ) {
-					continue;
-				}
-				cur--;
-				if( cur >= 0 &&
-					tokens[cur].first.Type == CToken::TType::Whitespace
-					) {
-					cur--;
-				}
-				while( cur >= 0 ) {
-					if( tokens[cur].first.Type == CToken::TType::Whitespace ) {
-						if( cur == 0 ) {
-							break;
-						}
-						cur--;
-					}
-					if( tokens[cur].first.Type == CToken::TType::Identifier ) {
-						identifiers.insert( tokens[cur].first.Text );
-						if( cur == 0 ) {
-							break;
-						}
-						cur--;
-					}
-					if( tokens[cur].first.Type == CToken::TType::Whitespace ) {
-						if( cur == 0 ) {
-							break;
-						}
-						cur--;
-					}
-					if(
-						tokens[cur].first.Type == CToken::TType::Other &&
-						tokens[cur].first.Text == ","
-						) {
-						if( cur == 0 ) {
-							break;
-						}
-						cur--;
-					} else {
-						break;
-					}
-				}
-			}	
 		}
 		return identifiers;
 	}
-
 
 }
--- Highlighting/IdentifierParser.h
+++ Highlighting/IdentifierParser.h
@@ -8,7 +8,18 @@
 namespace Yapynb {
 
 	class IdentifierParser {
+	private:
+		void processFor(
+			const std::vector<std::pair<CToken, size_t>>& tokens,
+			size_t cur,
+			std::unordered_set<std::string>& identifiers );
+
+		void processEquals(
+			const std::vector<std::pair<CToken, size_t>>& tokens,
+			size_t cur,
+			std::unordered_set<std::string>& identifiers );
 	public:
+
 		std::unordered_set<std::string> getIdentifiers( 
 			const std::vector<std::pair<CToken, size_t>>& tokens );
 
