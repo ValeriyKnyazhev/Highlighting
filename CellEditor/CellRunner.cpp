1f731498f585f9e52b1cda0f445437aa70f708b6
Sdernal <sdernal2@gmail.com>
2016-10-24 16:41:30 +0300

    CellRunner v1.3: embended dict, all base types added

--- CellEditor/CellRunner.cpp
+++ CellEditor/CellRunner.cpp
@@ -64,7 +64,24 @@ std::string CellRunner::answerBuilder( PyObject* val )
 		}
 		answer.append( answerBuilder( PyList_GetItem( val, listSize-1 ) ) );
 		answer.append( std::string( "]" ) );
-	} else {
+	} else if( PyDict_Check( val ) ) {
+		PyObject* keys = PyDict_Keys( val );
+		answer.append( std::string( "{" ) );
+		for( int i = 0; i < PyList_Size( keys ); i++ ) {
+			PyObject* key = PyList_GetItem( keys, i );
+			PyObject* dictval = PyDict_GetItem( val, key );
+			answer.append( std::string( "'" ) );
+			answer.append( std::string( PyUnicode_AsUTF8( key ) ) );
+			answer.append( std::string( "'" ) );
+			answer.append( " : " );
+			answer.append( answerBuilder( dictval ) );
+			if( i < PyList_Size( keys ) - 1 ) {
+				answer.append( std::string( ", " ) );
+			}
+		}
+		answer.append( std::string( "}" ) );
+	}
+	else {
 		answer.append( std::string( "None" ) );
 	}
 	answer.append( " \n" );
